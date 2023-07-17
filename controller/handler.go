89da16d397f8e901d8e7bb542eac06fa542ed018
izasoerya <ihza.surya@gmail.com>
2023-07-17 16:09:06 +0700

    add all folder

---
+++ controller/handler.go
@@ -0,0 +1,26 @@
+package controller
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func userInputURL(c *fiber.Ctx) error{
+	type Request struct {
+		URL string `json:"url"`
+	}
+	var body Request
+	err := c.BodyParser(&body)
+	if err!= nil {
+        fmt.Println(err)
+    }
+
+	url := body.URL
+	fmt.Println(url)
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":"OK",
+		"message": url,
+	})
+}
--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"fmt"
-	
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
-    fmt.Println("Hello, world!")
+	engine := html.New("./static", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Use(logger.New())
+	app.Get("/home", func(c *fiber.Ctx) error {						//* set index.html in /app
+		return c.Render("index", fiber.Map{})
+	})
+
+	app.Listen(":3000")
 }
