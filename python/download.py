ab4269e67856804846f56aa71a627afa92539c56
izasoerya <ihza.surya@gmail.com>
2023-08-31 16:34:12 +0700

    change name

---
+++ python/download.py
@@ -0,0 +1,18 @@
+# https://www.youtube.com/watch?v=rCLekTzKUrw&list=RDMMrCLekTzKUrw
+
+import sys
+from pytube import YouTube
+
+def Download(link, savePath):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download(savePath)
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+if __name__ == "__main__":
+    savePath = "/home/iza/Videos"
+    url = sys.argv[1]
+    Download(url, savePath)
